267d417073ba6cb169a33d1b46d5a8a7997ee545
Mohd Khairi Mohd Adnan <khairi@labs.my>
2016-08-31 16:49:58 +0800

    update generator

--- lib/generators/jquery/datatables/install_generator.rb
+++ lib/generators/jquery/datatables/install_generator.rb
@@ -13,10 +13,12 @@ module Jquery
         def add_assets
           js_manifest = 'app/assets/javascripts/application.js'
           css_manifest = 'app/assets/stylesheets/application.css'
-          js_strings = "//= require datatables"
+          scss_manifest = 'app/assets/stylesheets/application.scss'
+          js_strings = "//= require datatables\n"
 
           insert_into_file js_manifest, js_strings, :after => "jquery_ujs\n" if File.exists?(js_manifest)
           insert_css_strings(css_manifest) if File.exists?(css_manifest)
+          insert_scss_strings(scss_manifest) if File.exists?(scss_manifest)
           copy_core_file
         end
 
@@ -24,7 +26,7 @@ module Jquery
         
         def insert_css_strings(css_manifest)
           content = File.read(css_manifest)
-          css_strings = "*/= require datatables"
+          css_strings = "*/= require datatables\n"
 
           if requires_tree(content)
             insert_into_file css_manifest, css_strings, :after => "require_tree .\n"
@@ -33,9 +35,13 @@ module Jquery
           else
             insert_into_file css_manifest, css_strings, :before => " */"
           end
-          
-          
             
+        end
+
+        def insert_scss_strings(scss_manifest)
+          content = File.read(scss_manifest)
+          scss_strings = "@import \"datatables\";\n"
+          append_to_file scss_manifest, scss_strings
         end
 
         def requires_tree(content)
