b523dce68cf56bedc0d95826f6c60252f0c75502
Mohd Khairi Mohd Adnan <khairi@labs.my>
2019-11-01 20:39:35 +0800

    update assets

--- app/assets/javascripts/datatables/extensions/Buttons/dataTables.buttons.js
+++ app/assets/javascripts/datatables/extensions/Buttons/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 1.6.0
+/*! Buttons for DataTables 1.6.1
  * ©2016-2019 SpryMedia Ltd - datatables.net/license
  */
 
@@ -505,7 +505,8 @@ $.extend( Buttons.prototype, {
 			}
 
 			if ( built.conf.buttons ) {
-				built.collection = $('<div/>');
+				built.collection = $('<'+this.c.dom.collection.tag+'/>');
+
 				built.conf._collection = built.collection;
 
 				this._expandButton( built.buttons, built.conf.buttons, true, attachPoint );
@@ -969,9 +970,15 @@ $.extend( Buttons.prototype, {
 			close();
 		}
 
-		content = $(content);
+		var display = $('<div/>')
+			.addClass('dt-button-collection')
+			.addClass(options.collectionLayout)
+			.css('display', 'none');
 
-		var tableContainer = $( hostButton.table().container() );
+		content = $(content)
+			.addClass(options.contentClassName)
+			.attr('role', 'menu')
+			.appendTo(display);
 
 		hostNode.attr( 'aria-expanded', 'true' );
 
@@ -979,22 +986,15 @@ $.extend( Buttons.prototype, {
 			hostNode = document.body.lastChild;
 		}
 
-		var display = $('<' + options.tag + '/>')
-			.addClass(options.contentClassName)
-			.attr('role', 'menu');
-
 		if ( options.collectionTitle ) {
 			display.prepend('<div class="dt-button-collection-title">'+options.collectionTitle+'</div>');
 		}
 
 		display
-			.addClass( options.collectionLayout )
-			.css( 'display', 'none' )
-			.append( content )
 			.insertAfter( hostNode )
-			.stop()
 			.fadeIn( options.fade );
 
+		var tableContainer = $( hostButton.table().container() );
 		var position = display.css( 'position' );
 
 		if ( options.align === 'dt-container' ) {
@@ -1347,7 +1347,7 @@ Buttons.defaults = {
 		},
 		collection: {
 			tag: 'div',
-			className: 'dt-button-collection'
+			className: ''
 		},
 		button: {
 			// Flash buttons will not work with `<button>` in IE - it has to be `<a>`
@@ -1370,7 +1370,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '1.6.0';
+Buttons.version = '1.6.1';
 
 
 $.extend( _dtButtons, {
